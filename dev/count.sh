00a94f84fec18551d93fd815c919fedd7de0acf6
D-Se <2869830202@qq.com>
2022-02-23 05:14:46 +0800

    commit count utility

---
+++ dev/count.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash sh
+# count lines of code per contributor to github repo in main .rmd files
+contr () {
+  local perfile="false";
+  if [[ $1 = "-f" ]]; then
+  perfile="true";
+  shift;
+  fi;
+  if [[ $# -eq 0 ]]; then
+          echo "no files given!" 1>&2;
+        return 1;
+        else
+          local f; {
+            for f in "$@";
+            do
+            echo "$f";
+            git blame --show-email "$f" |
+              sed -nE 's/^[^ ]* *.<([^>]*)>.*$/: \1/p' |
+              sort | uniq -c | sort -r -nk1;
+            done
+          } | if [[ "$perfile" = "true" ]]; then
+        tee /tmp/s.txt;
+        else
+          tee /tmp/s.txt > /dev/null;
+        fi;
+        awk -v FS='*: *' '/^ *[0-9]/{sums[$2] += $1} END {
+        for (i in sums) printf("%7s : %s\n", sums[i], i)
+        }' /tmp/s.txt |
+          sort -r -nk1;
+        fi
+}
+cd ..
+find rmd | tail -n +2 | while read file; do contr -f "$file"; done > dev/counts.txt
