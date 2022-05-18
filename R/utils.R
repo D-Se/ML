c51be850831b5356bf95e39d0fa4ffcae345451f
D-Se <2869830202@qq.com>
2022-05-18 13:31:28 +0800

    add project dependencies convenience fun

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,32 @@
 # ML?
 
+#' install model packages and their dependencies
+#' 
+#' @importFrom utils installed.packages
+#' @importFrom utils install.packages
+#' @export
+install_dependencies <- function() {
+  deps <- tools::package_dependencies(
+    c(
+      "finetune", "glmnet", "earth", "kernlab",
+      "kknn", "xgboost", "baguette", "rules",
+      "cli", "crayon", "downlit", "lme4", 'tune', 'visNetwork',
+      "workflowsets"
+    ),
+    recursive = TRUE
+  ) |>
+    unlist(use.names = F) |>
+    unique()
+  
+  pkgs <- installed.packages()
+  to_install <- deps[!deps %in% pkgs]
+  
+  if(to_install) {
+    install.packages(to_install, F)
+  }
+  invisible()
+}
+
 #' Null-coalescing operator
 #' @noRd
 `%null%` <- function(x, y) if (is.null(x)) y else x
